5438dbf7d1420162bf90f734b3c8e6805ff4c61a
Doyeong Jung <sonatine789@naver.com>
2024-11-20 10:53:35 +0900

    feat: 로그인 및 회원가입 (nickname 추가), django-environ 설정 추가

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from movies.models import Actor, Director, Genre, Movie
+from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_username, user_email, user_field
 
 # Create your models here.
 class User(AbstractUser):
@@ -11,3 +13,30 @@ class User(AbstractUser):
     liked_directors = models.ManyToManyField(Director, related_name='liked_by_users')
     liked_genres = models.ManyToManyField(Genre, related_name='liked_by_users')
     liked_movies = models.ManyToManyField(Movie, related_name='liked_by_users')
+
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=False):
+        data = form.cleaned_data
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        username = data.get('username')
+        nickname = data.get('nickname')
+
+        user_email(user, email)
+        user_username(user, username)
+        if first_name:
+            user_field(user, 'first_name', first_name)
+        if last_name:
+            user_field(user, 'last_name', last_name)
+        if nickname:
+            user_field(user, 'nickname', nickname)
+        if "password1" in data:
+            user.set_password(data["password1"])
+        else:
+            user.set_unusable_password()
+        self.populate_username(request, user)
+        if commit:
+            user.save()
+        return user
