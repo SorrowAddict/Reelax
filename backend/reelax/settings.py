5e777d924abe5732df31ec3101b8a0364ee16091
Yeseung-Park <yspark8538@naver.com>
2024-11-20 11:12:31 +0900

    flx: cors headers

--- backend/reelax/settings.py
+++ backend/reelax/settings.py
@@ -80,6 +80,11 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",  # django-allauth
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
