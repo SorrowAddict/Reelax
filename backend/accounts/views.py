35e66ba7e736b50be728f1fcfd1bad72c60ff8ed
Doyeong Jung <sonatine789@naver.com>
2024-11-21 11:32:10 +0900

    feat: 유저 프로필 CRUD 기능 구현

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from .serializers import ProfileSerializer
 
 # Create your views here.
+class Profile(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
