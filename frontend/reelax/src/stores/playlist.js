787d8abaeae09acd16207c8c8887542fcd7a60a4
Yeseung-Park <yspark8538@naver.com>
2024-12-13 16:08:19 +0900

    fix: 상세정보 버그 수정

--- frontend/reelax/src/stores/playlist.js
+++ frontend/reelax/src/stores/playlist.js
@@ -5,7 +5,8 @@ import { useAccountStore } from './account'
 import { useRouter } from 'vue-router'
 
 export const usePlaylistStore = defineStore('playlist', () => {
-  const BASE_URL = 'http://43.203.255.151:8000/api/v1/movies'
+  const BASE_URL = 'http://localhost:8000/api/v1/movies'
+  // const BASE_URL = 'http://43.203.255.151:8000/api/v1/movies'
   const accountStore = useAccountStore()
   const router = useRouter()
   const playlist = ref({})
